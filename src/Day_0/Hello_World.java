e58e8c08283f8b8c79cf78ff6e2ca5b46b62e375
NIRANTH <niranth.y@gmail.com>
2018-07-15 22:18:51 +0530

    Added a new print line

--- src/Day_0/Hello_World.java
+++ src/Day_0/Hello_World.java
@@ -4,5 +4,6 @@ public class Hello_World {
     public static void main(String[] args) {
         System.out.println("Hello World. This is Niranth's First Java Program");
         System.out.println("Thanks to YouTube, for all the learning.");
+        System.out.println("Finally on GITHUB!!!");
     }
 }
